97186b6e1992914f38ce209512f221e198b2c8ff
jo <jo@banyaninfotech.com>
2017-03-15 00:22:14 +0530

    //jo mid night work

--- app/src/main/java/banyan/com/gemcrm/activity/Fragment_Dashboard.java
+++ app/src/main/java/banyan/com/gemcrm/activity/Fragment_Dashboard.java
@@ -6,9 +6,11 @@ package banyan.com.gemcrm.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import banyan.com.gemcrm.R;
 
@@ -18,6 +20,8 @@ public class Fragment_Dashboard extends Fragment {
     public Fragment_Dashboard() {
         // Required empty public constructor
     }
+
+    CardView card1, card2, card3, card4;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,47 @@ public class Fragment_Dashboard extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
 
-        // Inflate the layout for this fragment
+        card1 = (CardView) rootView.findViewById(R.id.dashboard_card_view1);
+        card2 = (CardView) rootView.findViewById(R.id.dashboard_card_view2);
+        card3 = (CardView) rootView.findViewById(R.id.dashboard_card_view3);
+        card4 = (CardView) rootView.findViewById(R.id.dashboard_card_view4);
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Target 1", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Target 2", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Target 3", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Target 4", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
         return rootView;
     }
 
@@ -44,4 +88,6 @@ public class Fragment_Dashboard extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+
 }
