e2c4b2684a3a87a9d46f6252343b11f33797b33c
steve <sharmila@banyaninfotech.com>
2017-03-15 18:34:51 +0530

    added campaign

--- app/src/main/java/banyan/com/gemcrm/activity/Fragment_Campaign.java
+++ app/src/main/java/banyan/com/gemcrm/activity/Fragment_Campaign.java
@@ -1,47 +1,76 @@
 package banyan.com.gemcrm.activity;
 
-import android.app.Activity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.fabtransitionactivity.SheetLayout;
+
 import banyan.com.gemcrm.R;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by steve on 14/3/17.
  */
 
-public class Fragment_Campaign extends Fragment {
+public class Fragment_Campaign extends Fragment implements SheetLayout.OnFabAnimationEndListener{
 
-    public Fragment_Campaign() {
-        // Required empty public constructor
+    SheetLayout mSheetLayout;
+    FloatingActionButton mFab;
+
+    private static final int REQUEST_CODE = 1;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_campaign, null);
+
+        ButterKnife.bind(getActivity());
+        // setUpToolbarWithTitle(getString(R.string.INBOX), false);
+
+        mFab = (FloatingActionButton) rootview.findViewById(R.id.fab_add_campaign);
+        mSheetLayout = (SheetLayout) rootview.findViewById(R.id.bottom_sheet2);
+
+        mSheetLayout.setFab(mFab);
+        mSheetLayout.setFabAnimationEndListener(this);
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSheetLayout.expandFab();
+            }
+        });
+
+        return rootview;
+
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    @OnClick(R.id.fab_add_campaign)
+    void onFabClick() {
+        mSheetLayout.expandFab();
     }
 
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_campaign, container, false);
+    public void onFabAnimationEnd() {
 
+        Intent intent = new Intent(getActivity(), Activity_Campaign_Add.class);
+        startActivityForResult(intent, REQUEST_CODE);
 
-        // Inflate the layout for this fragment
-        return rootView;
     }
-
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE){
+            mSheetLayout.contractFab();
+        }
     }
 }
