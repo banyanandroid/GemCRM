473fc32021be1f33893e6b2b8f85fd63fbf05d3c
jo <jo@banyaninfotech.com>
2017-03-15 19:22:07 +0530

    //jo mid night work

--- app/src/main/java/banyan/com/gemcrm/activity/Activity_Campaign_Add.java
+++ app/src/main/java/banyan/com/gemcrm/activity/Activity_Campaign_Add.java
@@ -1,11 +1,15 @@
 package banyan.com.gemcrm.activity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import banyan.com.gemcrm.R;
 import banyan.com.gemcrm.global.BaseActivity_Appoinment;
@@ -19,6 +23,8 @@ public class Activity_Campaign_Add extends BaseActivity_Appoinment implements Ad
 
     EditText edt_date;
 
+    int from_year, from_month, from_date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         enterFromBottomAnimation();
@@ -30,6 +36,41 @@ public class Activity_Campaign_Add extends BaseActivity_Appoinment implements Ad
         edt_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                final Calendar c = Calendar.getInstance();
+                from_year = c.get(Calendar.YEAR);
+                from_month = c.get(Calendar.MONTH);
+                from_date = c.get(Calendar.DAY_OF_MONTH);
+
+                // Launch Date Picker Dialog
+                DatePickerDialog dpd = new DatePickerDialog(
+                        Activity_Campaign_Add.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // Display Selected date in textbox
+
+                                int month = monthOfYear + 1;
+                                String formattedMonth = "" + month;
+                                String formattedDayOfMonth = "" + dayOfMonth;
+
+                                if (month < 10) {
+
+                                    formattedMonth = "0" + month;
+                                }
+                                if (dayOfMonth < 10) {
+
+                                    formattedDayOfMonth = "0" + dayOfMonth;
+                                }
+                                edt_date.setText(year + "-"
+                                        + formattedMonth + "-"
+                                        + formattedDayOfMonth);
+
+                            }
+                        }, from_year, from_month, from_date);
+                dpd.show();
 
             }
         });
